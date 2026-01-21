53866c9743920c44d4e849e37e6c81ed3d937b6a
dingwanren <154835434@qq.com>
2026-01-21 16:39:39 +0800

    feat: 添加github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 如果你使用的是项目页面 (例如: https://username.github.io/repo-name)，请取消注释下面的行并替换为你的仓库名称
+  base: '/demos/',
   plugins: [
     vue(),
     codeInspectorPlugin({
